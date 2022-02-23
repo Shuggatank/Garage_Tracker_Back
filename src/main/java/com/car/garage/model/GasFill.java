45d6e5f8b34107f9c7f764acabe3467eb1259e46
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-23 14:16:18 -0800

    create gasfill model

---
+++ src/main/java/com/car/garage/model/GasFill.java
@@ -0,0 +1,90 @@
+package com.car.garage.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "gas_fill")
+public class GasFill {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Date date;
+
+    @Column
+    private Double totalAmount;
+
+    @Column
+    private Double cost;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    public GasFill() {
+    }
+
+    public GasFill(Long id, Date date, Double totalAmount, Double cost) {
+        this.id = id;
+        this.date = date;
+        this.totalAmount = totalAmount;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "GasFill{" +
+                "id=" + id +
+                ", date=" + date +
+                ", totalAmount=" + totalAmount +
+                ", cost=" + cost +
+                ", vehicle=" + vehicle +
+                '}';
+    }
+}
