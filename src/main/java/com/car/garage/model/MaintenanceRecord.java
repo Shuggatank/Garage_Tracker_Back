e1b37a66cd8da3c20e544c8482b9d55e4df31701
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-23 14:23:15 -0800

    create maintenance record model

---
+++ src/main/java/com/car/garage/model/MaintenanceRecord.java
@@ -0,0 +1,91 @@
+package com.car.garage.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name  = "maintenance_record")
+public class MaintenanceRecord {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Column
+    private Double cost;
+
+    @Column
+    private Date date;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    public MaintenanceRecord() {
+    }
+
+    public MaintenanceRecord(Long id, String description, Double cost, Date date) {
+        this.id = id;
+        this.description = description;
+        this.cost = cost;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "MaintenanceRecord{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", cost=" + cost +
+                ", date=" + date +
+                ", vehicle=" + vehicle +
+                '}';
+    }
+}
