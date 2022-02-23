8fe445b503a416bc874de6692350e18b79cc6daf
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-23 15:26:26 -0800

    Get all vehicles mapping done for VehicleController.java.

--- src/main/java/com/car/garage/controller/VehicleController.java
+++ src/main/java/com/car/garage/controller/VehicleController.java
@@ -1,11 +1,30 @@
 package com.car.garage.controller;
 
+
+import com.car.garage.model.Vehicle;
+import com.car.garage.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api" )
 public class VehicleController {
 
+    private VehicleService vehicleService;
+
+    @Autowired
+    public void setVehicleService(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping("/vehicles")
+    public List<Vehicle> getAllVehicles() {
+        System.out.println("Calling getAllVehicles");
+        return vehicleService.getAllVehicles();
+    }
 
 }
