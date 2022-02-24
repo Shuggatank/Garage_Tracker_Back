485adf1e4de352357ce85baceaccc842fd0e341e
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-23 19:46:44 -0800

    Created GasFillController.java with get all and post mapping

---
+++ src/main/java/com/car/garage/controller/GasFillController.java
@@ -0,0 +1,33 @@
+package com.car.garage.controller;
+
+import com.car.garage.model.GasFill;
+import com.car.garage.model.OilChange;
+import com.car.garage.service.GasFillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vehicles" )
+public class GasFillController {
+
+    private GasFillService gasFillService;
+
+    @Autowired
+    public void setGasFillService (GasFillService gasFillService) {
+        this.gasFillService = gasFillService;
+    }
+
+    @GetMapping("/{vehicleId}/gas")
+    public List<GasFill> getAllGasFills(@PathVariable(value = "vehicleId") Long vehicleId) {
+        System.out.println("calling AllGasFills");
+        return gasFillService.getAllGasFills(vehicleId);
+    }
+
+    @PostMapping("/{vehicleId}/gas")
+    public GasFill createGasFill(@PathVariable (value = "vehicleId") Long vehicleId, @RequestBody GasFill gasFillObject) {
+        System.out.println("Creating Oil Change");
+        return gasFillService.createGasFill(vehicleId, gasFillObject);
+    }
+}
