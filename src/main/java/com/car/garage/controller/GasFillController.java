f263fb52253aea2aa474cfd9023c198f06864c20
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 14:57:58 -0800

    Trying specific address for CORS

--- src/main/java/com/car/garage/controller/GasFillController.java
+++ src/main/java/com/car/garage/controller/GasFillController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "/**")
+@CrossOrigin(origins = "https://garagetracker.herokuapp.com")
 @RequestMapping("/api/vehicles" )
 public class GasFillController {
 
